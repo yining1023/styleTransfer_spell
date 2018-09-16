ba9838c41962c367e6a5c5485ff3528873753c31
Yining Shi <ys2189@nyu.edu>
2018-09-16 18:31:19 -0400

    added example

---
+++ sketch.js
@@ -0,0 +1,50 @@
+// Copyright (c) 2018 ml5
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Style Transfer Image Example using p5.js
+This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young American Girl, The Dance)
+=== */
+
+let inputImg;
+let statusMsg;
+let transferBtn;
+let style1;
+
+function setup() {
+  noCanvas();
+  // Status Msg
+  statusMsg = select('#statusMsg');
+
+  // Get the input image
+  inputImg = select('#inputImg');
+
+  // Transfer Button
+  transferBtn = select('#transferBtn')
+  transferBtn.mousePressed(transferImages);
+
+  // Create two Style methods with different pre-trained models
+  style1 = ml5.styleTransfer('models/fuchun', modelLoaded);
+}
+
+// A function to be called when the models have loaded
+function modelLoaded() {
+  // Check if both models are loaded
+  if(style1.ready){
+    statusMsg.html('Ready!')
+  }
+}
+
+// Apply the transfer to both images!
+function transferImages() {
+  statusMsg.html('Applying Style Transfer...!');
+  
+  style1.transfer(inputImg, function(err, result) {
+    createImg(result.src).parent('output');
+  });
+
+  statusMsg.html('Done!');
+}
